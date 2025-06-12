55e721d859d70687b004984d6b0cc763ba6baca5
eduanlima@github.com <eduandev@gmail.com>
2025-06-12 20:58:58 -0300

    Project created

---
+++ src/test/java/br/com/posterius/spring_security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.posterius.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
