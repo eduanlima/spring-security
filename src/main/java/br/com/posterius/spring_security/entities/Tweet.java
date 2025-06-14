aa267c447eb4f91245a094ea4ee6411cc0c31f1d
eduanlima@github.com <eduandev@gmail.com>
2025-06-14 11:06:32 -0300

    Fix file docker and config database

--- src/main/java/br/com/posterius/spring_security/entities/Tweet.java
+++ src/main/java/br/com/posterius/spring_security/entities/Tweet.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -18,6 +20,8 @@ public class Tweet {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long tweetId;
+	@ManyToOne
+	@JoinColumn(name = "account_id")
 	private Account account;
 	private String content;
 	@CreationTimestamp
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO role (role_id, name) VALUES (1, 'admin');
+INSERT IGNORE INTO role (role_id, name) VALUES (2, 'basic');
