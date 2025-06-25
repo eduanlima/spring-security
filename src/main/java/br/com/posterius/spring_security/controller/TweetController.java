93df07083f7bd8d6b77b7a593675c2109d35da15
eduanlima@github.com <eduandev@gmail.com>
2025-06-24 22:22:20 -0300

    Function to allow admin user to delete tweets

--- src/main/java/br/com/posterius/spring_security/controller/TweetController.java
+++ src/main/java/br/com/posterius/spring_security/controller/TweetController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import br.com.posterius.spring_security.entities.Role;
 import br.com.posterius.spring_security.entities.Tweet;
 import br.com.posterius.spring_security.repositories.AccountRepository;
 import br.com.posterius.spring_security.repositories.TweetRepository;
@@ -42,7 +43,10 @@ public class TweetController {
 	private ResponseEntity<Void> delete(@PathVariable Long id, JwtAuthenticationToken token) {
 		var tweet = tweetRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		
-		if (!tweet.getAccount().getAccountId().equals(UUID.fromString(token.getName())))
+		var account = accountRepository.findById(UUID.fromString(token.getName()));
+		var isAdmin = account.get().getRoles().stream().anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+		
+		if (!isAdmin && !tweet.getAccount().getAccountId().equals(UUID.fromString(token.getName())))
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 		
 		tweetRepository.deleteById(id);
